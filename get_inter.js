2b292b31719528129e62e56d93ca0e47af339fa4
arroychaan <achmadroychan501@gmail.com>
2026-07-21 22:43:59 +0700

    chore: clean up unused test scripts and obsolete files

--- get_inter.js
+++
@@ -1,5 +0,0 @@
-async function getFont() {
-  const css = await fetch('https://fonts.googleapis.com/css2?family=Inter:wght@600').then(r => r.text());
-  console.log(css.match(/url\((.*?)\)/)[1]);
-}
-getFont();
